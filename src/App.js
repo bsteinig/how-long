c49f71c480222bf10f845f2c387a8907e418924b
Benjamin Steinig <bsteinig@umich.edu>
2022-02-04 01:59:57 -0500

    added gradients, created title animation, added animate.css package

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useState } from "react";
+import "animate.css";
+import "./styles/animations.css";
+import "./styles/app.css";
+import "./styles/gradients.css";
 
 function App() {
-  return <div className="App">Solstice</div>;
+  const [entrance, setEntrance] = useState("entrance");
+
+  const title = () => {
+    setEntrance(entrance ? "" : "entrance");
+  };
+
+  onanimationend = () => {
+    setEntrance(entrance ? "" : "entrance");
+  };
+
+  return (
+    <div className="main mirage" onClick={() => title()}>
+      <h1 className={`title ${entrance}`}>Solstice</h1>
+    </div>
+  );
 }
 
 export default App;
